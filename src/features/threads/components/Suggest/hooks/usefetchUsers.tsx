dbd1dae0651985a8eeb1d7baa332422026581291
taufik-hdyt <taufikhdyt259(@gmail.com>
2023-12-10 21:55:10 +0700

    updated

--- src/features/threads/components/Suggest/hooks/usefetchUsers.tsx
+++ src/features/threads/components/Suggest/hooks/usefetchUsers.tsx
@@ -2,19 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import { API } from "../../../../../libs/api";
 
 export const useFetchUsersSuggest = () => {
-  const { data: users, isLoading,refetch} = useQuery({
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryFn: async () => {
-      const dataUsers = await API.get("/suggest-users", {headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }});
-      return dataUsers.data
-
+      const dataUsers = await API.get("/suggest-users", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      return dataUsers.data;
     },
-    queryKey: ['fetch.users'],
+    queryKey: ["fetchUsers"],
   });
   return {
     data: users,
     isLoading,
-    refetch
+    refetch,
   };
 };
--- src/features/user/useFetchAllUsers.tsx
+++ src/features/user/useFetchAllUsers.tsx
@@ -2,16 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import { API } from "../../libs/api";
 
 export const useFetchAllUsers = (search?: string | "") => {
-  const { data: users, isLoading,refetch} = useQuery({
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryFn: async () => {
-      const dataUsers = await API.get(`/users?search=${search}`);
-      return dataUsers.data
+      const dataUsers = await API.get(`/users?search=${search}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      return dataUsers.data;
     },
-    queryKey: ['users'],
+    queryKey: ["users"],
   });
   return {
     data: users,
     isLoading,
-    refetch
+    refetch,
   };
 };
