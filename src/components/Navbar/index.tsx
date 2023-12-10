4c3e774e302f203fbd76d57e3bf239d4370bbb68
taufik-hdyt <taufikhdyt259(@gmail.com>
2023-12-11 04:46:50 +0700

    updated

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,6 +6,7 @@ import {
   Stack,
   Text,
   useDisclosure,
+  Link,
 } from "@chakra-ui/react";
 import { AiOutlineUser, AiOutlineHeart, AiOutlineHome } from "react-icons/ai";
 import { TbUserSearch } from "react-icons/tb";
@@ -14,7 +15,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/type/RootState";
 import ALertConfirm from "../Alert/ALert";
 import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AUTH_LOGOUT } from "../../store/RootReducer";
 
 function Navbar() {
@@ -34,25 +35,25 @@ function Navbar() {
       <Box>
         <Heading color="green">circle</Heading>
         <Stack mt={8} spacing={6}>
-          <Link to="/">
+          <Link href="/">
             <HStack cursor="pointer" color="white">
               <AiOutlineHome size={25} />
               <Text fontSize="sm  ">Home</Text>
             </HStack>
           </Link>
-          <Link to="/search">
+          <Link href="/search">
             <HStack cursor="pointer" color="white">
               <TbUserSearch size={25} />
               <Text fontSize="sm  ">Search</Text>
             </HStack>
           </Link>
-          <Link to="/follows">
+          <Link href="/follows">
             <HStack cursor="pointer" color="white">
               <AiOutlineHeart color="transparant" size={25} />
               <Text fontSize="sm  ">Follows</Text>
             </HStack>
           </Link>
-          <Link to={`profile/${auth.user.id}`}>
+          <Link href={`profile/${auth.user.id}`}>
             <HStack cursor="pointer" color="white">
               <AiOutlineUser size={25} />
               <Text fontSize="sm  ">Profile</Text>
